5b6cd2fbb04eb23f2d706c35ac57c3fdd06f319b
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-24 20:43:59 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
 
-  validate :is_title_case 
-  before_validation :make_title_case 
+  validate :is_title_case
+  before_validation :make_title_case
   belongs_to :author
 
   #put new code here
@@ -12,7 +12,7 @@ end
 def self.from_today
   where("created_at >=?", Time.zone.today.beginning_of_day)
 end
- 
+
 def self.old_news
   where("created_at <?", Time.zone.today.beginning_of_day)
 end
